2dd6bc33397fae1e62e101f72bbada66a2b9fd84
Atsushi Watanabe <atsushi.w@ieee.org>
2018-06-12 15:55:48 +0900

    Initial drop from neonavigation meta-package

---
+++ .travis/test.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -o errexit
+set -o verbose
+
+pip install gh-pr-comment
+
+source /opt/ros/${ROS_DISTRO}/setup.bash
+source /catkin_ws/devel/setup.bash
+
+cd /catkin_ws
+
+sync
+
+sed -i -e '5a set(CMAKE_C_FLAGS "-Wall -Werror -O2")' \
+  /opt/ros/${ROS_DISTRO}/share/catkin/cmake/toplevel.cmake
+sed -i -e '5a set(CMAKE_CXX_FLAGS "-Wall -Werror -O2")' \
+  /opt/ros/${ROS_DISTRO}/share/catkin/cmake/toplevel.cmake
+
+CM_OPTIONS=""
+
+catkin_make ${CM_OPTIONS} \
+  || (gh-pr-comment FAILED '```catkin_make``` failed'; false)
+catkin_make tests ${CM_OPTIONS} \
+  || (gh-pr-comment FAILED '```catkin_make tests``` failed'; false)
+catkin_make run_tests ${CM_OPTIONS} \
+  || (gh-pr-comment FAILED '```catkin_make run_tests``` failed'; false)
+
+if [ catkin_test_results ];
+then
+  result_text="
+\`\`\`
+`catkin_test_results --all | grep -v Skipping || true`
+\`\`\`
+"
+else
+  result_text="
+\`\`\`
+`catkin_test_results --all | grep -v Skipping || true`
+\`\`\`
+`find build/test_results/ -name *.xml | xargs -n 1 -- bash -c 'echo; echo \#\#\# $0; echo; echo \\\`\\\`\\\`; xmllint --format $0; echo \\\`\\\`\\\`;'`
+"
+fi
+catkin_test_results || (gh-pr-comment FAILED "Test failed$result_text"; false)
+
+gh-pr-comment PASSED "All tests passed$result_text"
+
+cd ..
+rm -rf /catkin_ws || true
