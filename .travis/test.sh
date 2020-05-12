adc1ec36da03c4afe892f97468b1dc96a98dc64c
Atsushi Watanabe <atsushi.w@ieee.org>
2020-05-12 14:50:06 +0900

    Add Noetic CI job and prerelease test jobs (#32)
    * Add Noetic CI job and prerelease test jobs
    * Update CI script

--- .travis/test.sh
+++ .travis/test.sh
@@ -5,10 +5,11 @@ set -o errexit
 source /opt/ros/${ROS_DISTRO}/setup.bash
 cd /catkin_ws
 
+build_number="[[#${TRAVIS_BUILD_NUMBER}](${TRAVIS_BUILD_WEB_URL})]"
 
 pkgs=$(find . -name package.xml | xargs -n1 dirname)
 catkin_lint $pkgs \
-  || (gh-pr-comment "[#${TRAVIS_BUILD_NUMBER}] FAILED on ${ROS_DISTRO}" \
+  || (gh-pr-comment "${build_number} FAILED on ${ROS_DISTRO}" \
       "<details><summary>catkin_lint failed</summary>
 
 \`\`\`
@@ -21,14 +22,14 @@ sed -i -e '5a set(CMAKE_C_FLAGS "-Wall -Werror -O2")' \
 sed -i -e '5a set(CMAKE_CXX_FLAGS "-Wall -Werror -O2")' \
   /opt/ros/${ROS_DISTRO}/share/catkin/cmake/toplevel.cmake
 
-CM_OPTIONS=${CM_OPTIONS:-}
+CM_OPTIONS=""
 
 catkin_make ${CM_OPTIONS} \
-  || (gh-pr-comment "[#${TRAVIS_BUILD_NUMBER}] FAILED on ${ROS_DISTRO}" '```catkin_make``` failed'; false)
+  || (gh-pr-comment "${build_number} FAILED on ${ROS_DISTRO}" '```catkin_make``` failed'; false)
 catkin_make tests ${CM_OPTIONS} \
-  || (gh-pr-comment "[#${TRAVIS_BUILD_NUMBER}] FAILED on ${ROS_DISTRO}" '```catkin_make tests``` failed'; false)
+  || (gh-pr-comment "${build_number} FAILED on ${ROS_DISTRO}" '```catkin_make tests``` failed'; false)
 catkin_make run_tests ${CM_OPTIONS} \
-  || (gh-pr-comment "[#${TRAVIS_BUILD_NUMBER}] FAILED on ${ROS_DISTRO}" '```catkin_make run_tests``` failed'; false)
+  || (gh-pr-comment "${build_number} FAILED on ${ROS_DISTRO}" '```catkin_make run_tests``` failed'; false)
 
 if [ catkin_test_results ];
 then
@@ -45,10 +46,10 @@ $(catkin_test_results --all | grep -v Skipping || true)
 $(find build/test_results/ -name *.xml | xargs -n 1 -- bash -c 'echo; echo \#\#\# $0; echo; echo \\\`\\\`\\\`; xmllint --format $0; echo \\\`\\\`\\\`;')
 "
 fi
-catkin_test_results || (gh-pr-comment "[#${TRAVIS_BUILD_NUMBER}] FAILED on ${ROS_DISTRO}" "<details><summary>Test failed</summary>
+catkin_test_results || (gh-pr-comment "${build_number} FAILED on ${ROS_DISTRO}" "<details><summary>Test failed</summary>
 
 $result_text</details>"; false)
 
-gh-pr-comment "[#${TRAVIS_BUILD_NUMBER}] PASSED on ${ROS_DISTRO}" "<details><summary>All tests passed</summary>
+gh-pr-comment "${build_number} PASSED on ${ROS_DISTRO}" "<details><summary>All tests passed</summary>
 
 $result_text</details>" || true
